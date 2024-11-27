458e6a623395c6aa6322f538caa81a96875de8ee
adrian saldana <saldana5417@student.laccd.edu>
2024-11-26 18:41:37 -0800

    finished (late)

--- src/main/java/Exercise_1.java
+++ src/main/java/Exercise_1.java
@@ -1,43 +1,47 @@
 class Exercise_1 {
 
+
+    //AFTER THE FIX
+
+    
     //number of elements we want to check for
     public static int numElements = 100;
 
     public static void main(String[] args) {
 
         // Check if given a single argument
-        if(args.length == 1) {
+        if(args.length != 1) {
             System.out.println("./Exercise_1 value");
             return;
         }
 
-        int n = args[0];
+        int n = Integer.parseInt(args[0]);
 
         //create new array of numElements long
-        int[] arr = new int[numElement];
+        int[] arr = new int[numElements];
 
         //populate array from 0 to numElements
-        for(int i = 0; i <= numElements; i++) {
+        for(int i = 0; i < numElements; i++) {
             arr[i] = i;
         }
         
-        return;
-
         //find Divisable by n
         findDivisableBy(arr, n);
+
+        return;
     }
 
     public static void findDivisableBy(int arr[], int n) {
 
         // for every element of the array
-        for(int i = 0; i <= numElements; i==i+1) {
+        for(int i = 0; i < numElements; i=i+1) {
 
             // check if element is divisible by n
-            if((arr[i] % n) = 0) {
+            if((arr[i] % n) == 0) {
                 //print if divisibile
-                System.out.print arr[i] + ",")
+                System.out.print ( arr[i] + ",");
             }
         }
-        System.out.printIn("");
+        System.out.println("");
     }
 }
